55ecf718366a30caf12efafd010366d592569786
luandstv <luantensa@gmail.com>
2020-06-18 22:41:29 -0300

    correçao do desafio

--- desafioaposentadoria.js
+++ desafioaposentadoria.js
@@ -20,15 +20,18 @@ const soma2 = clientes[1].idade + clientes[1].contribuicao
 if(clientes[0].sexo == "F"){
  if (clientes[0].contribuicao >= 30 && soma1 >= 85) {
       console.log(`${clientes[0].nome}, você pode se aposentar. `)
-     }
-   } else {
-     console.log(`${clientes[0].nome}, você ainda nao pode se aposentar. `)
+     
+    
+    }else {
+      console.log(`${clientes[0].nome}, você ainda nao pode se aposentar. `)
+    } 
    } 
 
    if(clientes[1].sexo == "M"){
      if (clientes[1].contribuicao >= 35 && soma1 >= 95) {
           console.log(`${clientes[1].nome}, você pode se aposentar. `)
          }
-       } else {
-         console.log(`${clientes[1].nome}, você ainda nao pode se aposentar. `)
+         else {
+          console.log(`${clientes[1].nome}, você ainda nao pode se aposentar. `)
+        } 
        } 
